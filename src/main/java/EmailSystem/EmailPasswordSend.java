38e223bee47495ca815fbcc599a306f5bbbb2085
Midnight9385 <91230751+Midnight9385@users.noreply.github.com>
2023-09-14 22:08:13 -0500

    Revert "fixed security issue"
    This reverts commit 63c89900baed41abf540baacaf3c1211a0bd385b.

--- src/main/java/EmailSystem/EmailPasswordSend.java
+++ src/main/java/EmailSystem/EmailPasswordSend.java
@@ -8,23 +8,24 @@ import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 
 import de.milchreis.uibooster.components.Notification;
-import util.KeyReader;
+
+
 
 import java.io.IOException;
 
 public class EmailPasswordSend {
-  final static String emailServerKey = KeyReader.getAccessKey();
+  final static String emailServerPassword = System.getenv("APIKEY");
   final static Email from = new Email("diaryappnoreply@gmail.com");
   final static String subject = "Login Info";
 
   public static void main(String[] args) throws IOException {
-    System.out.println(emailServerKey);
+    System.out.println(emailServerPassword);
     
-    Email to = new Email("diaryappnoreply@gmail.com");
+    Email to = new Email("zacharynewkirk88@gmail.com");
     Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
     Mail mail = new Mail(from, subject, to, content);
 
-    SendGrid sg = new SendGrid(emailServerKey);
+    SendGrid sg = new SendGrid(emailServerPassword);
     Request request = new Request();
     try {
       request.setMethod(Method.POST);
@@ -40,18 +41,22 @@ public class EmailPasswordSend {
   }
 
   public static void sendEmail(String email, String password, String username){
-    System.out.println(emailServerKey);
+    System.out.println(emailServerPassword);
     // System.out.println("starting send method");
     Email to = new Email(email);
     Content content = new Content("text/plain", "Here is your login info \n\n username: "+username+"\tpassword: "+password);
     Mail mail = new Mail(from, subject, to, content);
 
-    SendGrid sg = new SendGrid(emailServerKey);
+    SendGrid sg = new SendGrid(emailServerPassword);
     Request request = new Request();
     try {
       request.setMethod(Method.POST);
       request.setEndpoint("mail/send");
       request.setBody(mail.build());
+      Response response = sg.api(request);
+      // System.out.println(response.getStatusCode());
+      // System.out.println(response.getBody());
+      // System.out.println(response.getHeaders());
     } catch (IOException ex) {
     }
     Notification n = new Notification();
